c42fc90135be7ddbcf4c98584b20cde7dede9b9d
Dougla Mamede <dogaohappy@gmail.com>
2024-11-12 15:55:51 -0300

    GET game by id

--- src/main/java/com/douglasmamede/dslist/services/GameService.java
+++ src/main/java/com/douglasmamede/dslist/services/GameService.java
@@ -2,12 +2,14 @@ package com.douglasmamede.dslist.services;
 
 import java.util.List;
 
+import com.douglasmamede.dslist.dto.GameDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.douglasmamede.dslist.dto.GameMinDTO;
 import com.douglasmamede.dslist.entities.Game;
 import com.douglasmamede.dslist.repositories.GameRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class GameService {
@@ -15,6 +17,13 @@ public class GameService {
   @Autowired
   private GameRepository gameRepository;
 
+  @Transactional(readOnly = true)
+  public GameDTO findById(Long id) {
+    Game result = gameRepository.findById(id).get();
+    return new GameDTO(result);
+  }
+
+  @Transactional(readOnly = true)
   public List<GameMinDTO> findAll() {
     List<Game> result = gameRepository.findAll();
     return result.stream().map(x -> new GameMinDTO(x)).toList();
